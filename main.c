4b28bf48222e284ef79fa4ce449cfc5ea0c40554
Slurfie <46998680+Slurfie@users.noreply.github.com>
2019-05-23 17:48:25 +0200

    Add files via upload
    changed the usage message

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kblum <kblum@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/27 12:23:27 by rsteigen       #+#    #+#                */
-/*   Updated: 2019/05/01 17:52:00 by kblum         ########   odam.nl         */
+/*   Updated: 2019/05/23 17:25:34 by kblum         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int     main(int ac, char **av)
     if (ac == 2 || ac == 4)
       entry(av, ac);
     else
-      ft_putstr("Usage: /fractol \"mandelbrot\", \"julia\", \"unknown\"\n, option: winwith winheight");
+      ft_putstr("Usage: /fractol \"mandelbrot\", \"julia\", \"unknown\", option: winwith winheight\n");
     return (0); 
 }
